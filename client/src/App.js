503aef3f10eadec15e7871bfb7ad408b69e400ac
Zachary Maier <zmaier416@gmail.com>
2023-02-09 15:07:28 -0600

    multiple buttons lol

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,25 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { useState } from 'react';
 
-export default function Pointing() {
+const points = [1,2,3,5,8,13];
+
+function ButtonLabeller(){
+  
+}
+
+function ButtonCounter(){
   const [count,setCount] = useState(0);
-  function YouVoted() {
-    alert('your expertise has been considered');
-  }
-  function handleClick() {
-    setCount(count +1);
-  }
-  return (
-    <div>
-      <h1>Welcome to Standup</h1>
-      <PointingOne count={count} onClick={handleClick} />
-      <PointingTwo count={count} onClick={handleClick} />
-    </div>
-  );
-}
-
-function PointingOne({ count, onClick}) {
-  return (
-    <button onClick={onClick}>
-      Clicked {count} times
-    </button>
-  );
-}
-
-function PointingTwo({ count, onClick}) {
   return(
-    <button onClick={onClick}>
-      Clicked {count} times
+    <button onClick={() => setCount(count + 1)}>
+      {count}
     </button>
+  );
+}
+
+export default function Pointing(){
+  return(
+    <div>
+      <h1>Story Pointing</h1>
+      {points.map(() => <ButtonCounter/>)}
+    </div>
   );
 }
